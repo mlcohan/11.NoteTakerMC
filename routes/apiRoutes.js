40b06720185ef5e0e795f5a7fe6fa01251c75ea2
Mary Cohan <mary.cohan@gmail.com>
2021-03-08 20:01:35 -0800

    set up

---
+++ routes/apiRoutes.js
@@ -0,0 +1,48 @@
+// LOAD DATA
+// We are linking our routes to a db sources.
+
+const db = require('../db/db');
+
+
+// ROUTING
+
+module.exports = (app) => {
+  // API GET Requests
+
+
+  app.get('/api/tables', (req, res) => res.json(tableData));
+
+  app.get('/api/waitlist', (req, res) => res.json(waitListData));
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post('/api/tables', (req, res) => {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+    if (tableData.length < 5) {
+      tableData.push(req.body);
+      res.json(true);
+    } else {
+      waitListData.push(req.body);
+      res.json(false);
+    }
+  });
+
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post('/api/clear', (req, res) => {
+    // Empty out the arrays of data
+    tableData.length = 0;
+    waitListData.length = 0;
+
+    res.json({ ok: true });
+  });
+};
