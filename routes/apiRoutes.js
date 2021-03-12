2edcfad369e8b1b9d67af3a735443adc9bef05fb
Mary Cohan <mary.cohan@gmail.com>
2021-03-12 15:09:09 -0800

    found bug in apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,78 +1,41 @@
+//DEPENDENCIES
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
-// LOAD DATA
+let db = require("../db/db.json");
 
-const path = require('path')
-const fs = require("fs")
-// // const store = require("../db/store")
-const storedData = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8"))
+//ROUTING
+module.exports = (app) => {
 
+  //get old notes
+    app.get("/api/notes", function (req,res){
+        res.json(db);
+    });
 
+    //make new notes
+    app.post("/api/notes", function (req,res){
+        const myNote = req.body;
+        myNote.id = uuidv4(myNote.id);
+        db.push(myNote);
 
-module.exports = app => {
- 
-//   // fs.readFile("db/db.json","utf8", (err, data) => {
-
-//   //     if (err) throw err;
-
-//   //     var notes = JSON.parse(data);
-//   //   console.log(notes)
-   
-       app.get("api/notes", function(req, res) {
-//         console.log("hi")
-           res.json(storedData);
-       });
-
-    
-       app.post("api/notes", function(req, res) {
-           let newNote = req.body;
-           storedData.push(newNote);
-           fs.writeFile("db/db.json",JSON.stringify(storedData),err => {
-             if (err) throw err;
-             return true;
-           })
-           return
-       });
-
-    
-       app.get("/notes/:id", function(req,res) {
-           res.json(storedData[req.params.id]);
-       });
-
-//       // app.delete("/api/notes/:id", function(req, res) {
-//       //     notes.splice(req.params.id, 1);
-//       //     fs.writeFile("db/db.json",JSON.stringify(notes),err => {
-//       //       if (err) throw err;
-//       //       return true;
-//       //     })
-//       // });
-//     // })
-   }
-
-
-// // // ROUTING
-
-// //  module.exports = (app) => {
-
-
-// // app.get("/api/notes", (req, res) => {
-
-// //   res.json(storedData)
-// // })
-
-// // app.post("/api/notes", (req, res) =>{
-
-// //   const nextNote = req.body
-
-// //   storedData.push(req.body)
-
-// //   fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(nextNote))
-
-// //   res.json(true)
-// //  })
-// // }
-
-// // // / app.delete('/api/notes/:id', (req, res) => {
+        fs.writeFile("db/db.json", JSON.stringify(db), (err)=>{
+            if (err) throw err;
+        });
+        
+        res.json(true);
+    });
   
-// // // }
+    //delete notes
+    app.delete("/api/notes/:id", function (req,res){
+        const myNoteId = req.params.id;
+        db = db.filter((notes, index)=>{
+            return myNoteId !== notes.id;
+        });
 
+        fs.writeFile("db/db.json", JSON.stringify(db), (err)=>{
+            if (err) throw err;
+        });
 
+        res.json(true);
+    });
+};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,9 +4,7 @@ const path = require('path');
 // ROUTING
 module.exports = (app) => {
   // => HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-
+ 
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
