bbcf33171c45ccc25de248db858bbb356c7ae4f4
Mary Cohan <mary.cohan@gmail.com>
2021-03-11 20:36:08 -0800

    working out bugs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,45 +1,78 @@
 // LOAD DATA
 
+const path = require('path')
+const fs = require("fs")
+// const store = require("../db/store")
+// const storedData = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8"))
 
-const fs = require('fs')
 
 
-// ROUTING
-
-module.exports = (app) => {
-
-  
-  fs.readFile('db/db.json', (err, data) => {
-    if (err) throw err;
-
-var notes = JSON.parse(data)
-
-app.get('/api/notes', (req, res) => res.json(notes));
-  
-
+module.exports = app => {
 
  
+  fs.readFile("db/db.json","utf8", (err, data) => {
 
-  app.post('/api/notes', (req, res) => {
+      if (err) throw err;
+
+      var notes = JSON.parse(data);
+
+   
+      app.get("/api/notes", function(req, res) {
+          res.json(notes);
+      });
+
     
-   const newNote = req.body
+      app.post("/api/notes", function(req, res) {
+          let newNote = req.body;
+          notes.push(newNote);
+          fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
+            if (err) throw err;
+            return true;
+          })
+          return console.log("Added new note: "+newNote.title);
+      });
+
     
-   notes.push(newNote);
-      
-   fs.writeFile("db/db.json", JSON.stringify(notes), err => {
-         if (err) throw err
-         return true
-       })
-      res.json(true)
+      app.get("/api/notes/:id", function(req,res) {
+          res.json(notes[req.params.id]);
+      });
+
+      app.delete("/api/notes/:id", function(req, res) {
+          notes.splice(req.params.id, 1);
+          fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
+            if (err) throw err;
+            return true;
+          })
+          console.log("Deleted note with id "+req.params.id);
+      });
     })
-    
-  });
+  }
 
 
-  // app.post('/api/clear', (req, res) => {
-  //   // Empty out the arrays of data
-  //   notes.length = 0;
+// // ROUTING
 
-  //   res.json({ ok: true });
-  // });
-};
+//  module.exports = (app) => {
+
+
+// app.get("/api/notes", (req, res) => {
+
+//   res.json(storedData)
+// })
+
+// app.post("/api/notes", (req, res) =>{
+
+//   const nextNote = req.body
+
+//   storedData.push(req.body)
+
+//   fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(nextNote))
+
+//   res.json(true)
+//  })
+// }
+
+// // / app.delete('/api/notes/:id', (req, res) => {
+  
+// // }
+
+
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // DEPENDENCIES
 // Series of npm packages that we will use to give our server useful functionality
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
-const db = require("./db/db");
+// const fs = require("fs");
+// const path = require("path");
+// const db = require("./db/db");
 
 
 // Tells node that we are creating an "express" server
@@ -11,13 +11,12 @@ const app = express();
 // Sets an initial port. We"ll use this later in our listener
 const PORT = process.env.PORT || 8080;
 
-let notes = []
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname)));
-
+// app.use(express.static(path.join(__dirname)));
+app.use(express.static('public'));
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
