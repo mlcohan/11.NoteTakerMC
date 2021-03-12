717dedf1a13ed2b2f305b98b912f21066cf10f6c
Mary Cohan <mary.cohan@gmail.com>
2021-03-11 22:32:38 -0800

    fixing error messages

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,78 +1,78 @@
+
 // LOAD DATA
 
 const path = require('path')
 const fs = require("fs")
-// const store = require("../db/store")
-// const storedData = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8"))
+// // const store = require("../db/store")
+const storedData = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8"))
 
 
 
 module.exports = app => {
-
  
-  fs.readFile("db/db.json","utf8", (err, data) => {
+//   // fs.readFile("db/db.json","utf8", (err, data) => {
 
-      if (err) throw err;
+//   //     if (err) throw err;
 
-      var notes = JSON.parse(data);
-
+//   //     var notes = JSON.parse(data);
+//   //   console.log(notes)
    
-      app.get("/api/notes", function(req, res) {
-          res.json(notes);
-      });
+       app.get("api/notes", function(req, res) {
+//         console.log("hi")
+           res.json(storedData);
+       });
 
     
-      app.post("/api/notes", function(req, res) {
-          let newNote = req.body;
-          notes.push(newNote);
-          fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
-            if (err) throw err;
-            return true;
-          })
-          return console.log("Added new note: "+newNote.title);
-      });
+       app.post("api/notes", function(req, res) {
+           let newNote = req.body;
+           storedData.push(newNote);
+           fs.writeFile("db/db.json",JSON.stringify(storedData),err => {
+             if (err) throw err;
+             return true;
+           })
+           return
+       });
 
     
-      app.get("/api/notes/:id", function(req,res) {
-          res.json(notes[req.params.id]);
-      });
+       app.get("/notes/:id", function(req,res) {
+           res.json(storedData[req.params.id]);
+       });
 
-      app.delete("/api/notes/:id", function(req, res) {
-          notes.splice(req.params.id, 1);
-          fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
-            if (err) throw err;
-            return true;
-          })
-          console.log("Deleted note with id "+req.params.id);
-      });
-    })
-  }
-
-
-// // ROUTING
-
-//  module.exports = (app) => {
+//       // app.delete("/api/notes/:id", function(req, res) {
+//       //     notes.splice(req.params.id, 1);
+//       //     fs.writeFile("db/db.json",JSON.stringify(notes),err => {
+//       //       if (err) throw err;
+//       //       return true;
+//       //     })
+//       // });
+//     // })
+   }
 
 
-// app.get("/api/notes", (req, res) => {
+// // // ROUTING
 
-//   res.json(storedData)
-// })
+// //  module.exports = (app) => {
 
-// app.post("/api/notes", (req, res) =>{
 
-//   const nextNote = req.body
+// // app.get("/api/notes", (req, res) => {
 
-//   storedData.push(req.body)
+// //   res.json(storedData)
+// // })
 
-//   fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(nextNote))
+// // app.post("/api/notes", (req, res) =>{
 
-//   res.json(true)
-//  })
-// }
+// //   const nextNote = req.body
 
-// // / app.delete('/api/notes/:id', (req, res) => {
-  
+// //   storedData.push(req.body)
+
+// //   fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(nextNote))
+
+// //   res.json(true)
+// //  })
 // // }
+
+// // // / app.delete('/api/notes/:id', (req, res) => {
+  
+// // // }
 
 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,7 @@
 // DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-
 const path = require('path');
 
 // ROUTING
-
 module.exports = (app) => {
   // => HTML GET Requests
   // Below code handles when users "visit" a page.
